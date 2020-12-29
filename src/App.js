f853ae11302e6bd7582ec1be810cf1379bc8d979
Mateusz Bogolubow <mateusz@bogolubow.com>
2020-12-29 11:20:42 +0100

    Add info about devmentor.pl

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p><small>v{process.env.REACT_APP_VERSION}</small></p>
+        <p>
+            <small>v{process.env.REACT_APP_VERSION}</small></p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with <a href="https://devmentor.pl">devmentor.pl</a>
         </a>
 
       </header>
