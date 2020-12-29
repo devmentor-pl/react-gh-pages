747424efd76f60de7c406f2b669ff78adfcd4336
Mateusz Bogolubow <mateusz@bogolubow.com>
2020-12-29 10:40:20 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p><small>v{process.env.REACT_APP_VERSION}</small></p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +18,9 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
+      
     </div>
   );
 }
